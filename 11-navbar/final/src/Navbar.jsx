1361c412e7effbdbe4c0873bd8a8ecc3aab80871
smajaas <smajaas@gmail.com>
2023-03-31 07:14:46 +0530

    navbar completed

--- 11-navbar/final/src/Navbar.jsx
+++ 11-navbar/final/src/Navbar.jsx
@@ -43,6 +43,18 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
+        {/* social links */}
+        <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
