bd8a13d25131b8de63b1bbf953ece4075884706c
smajaas <smajaas@gmail.com>
2023-03-31 06:48:04 +0530

    navbar dynamic

--- 11-navbar/final/src/Navbar.jsx
+++ 11-navbar/final/src/Navbar.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
-  const toggleLink = () => {
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
     setShowLinks(!showLinks);
+  };
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
   };
 
   return (
@@ -14,24 +22,27 @@ const Navbar = () => {
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} className='logo' alt='logo' />
-          <button className='nav-toggle' onClick={toggleLink}>
+          <button className='nav-toggle' onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className='links-container'>
-            <ul className='links'>
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div
+          className='links-container'
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
