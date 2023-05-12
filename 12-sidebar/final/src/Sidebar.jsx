808ac0e591a7235cda3b3b0a026f07f6d6cf3093
smajaas <smajaas@gmail.com>
2023-05-12 17:10:31 +0530

    sidebar open close

--- 12-sidebar/final/src/Sidebar.jsx
+++ 12-sidebar/final/src/Sidebar.jsx
@@ -7,7 +7,7 @@ import { FaTimes } from 'react-icons/fa';
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <aside className='sidebar'>
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
       <div className='sidebar-header'>
         <img src={logo} alt='logo name' className='logo' />
         <button className='close-btn' onClick={closeSidebar}>
@@ -27,7 +27,7 @@ const Sidebar = () => {
           );
         })}
       </ul>
-      <ul className='social-links'>
+      <ul className='social-icons'>
         {social.map((link) => {
           const { id, url, icon } = link;
           return (
