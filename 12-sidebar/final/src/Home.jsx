68adf7d48c68be88c8e02fe539589866f9641dbb
smajaas <smajaas@gmail.com>
2023-05-12 12:25:09 +0530

    css

--- 12-sidebar/final/src/Home.jsx
+++ 12-sidebar/final/src/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useGlobalContext } from './context';
+import { FaBars } from 'react-icons/fa';
 
 const Home = () => {
-  return <div>Home</div>;
+  const { openSidebar, openModal } = useGlobalContext();
+  console.log(openSidebar);
+  return (
+    <main>
+      <button onClick={openSidebar} className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button onClick={openModal} className='btn'>
+        show modal
+      </button>
+    </main>
+  );
 };
 
 export default Home;
