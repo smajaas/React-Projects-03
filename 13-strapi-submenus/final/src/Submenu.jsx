515e3acf0a21e82b675efd0084d120cf6b405c71
smajaas <smajaas@gmail.com>
2023-05-14 00:32:04 +0530

    submenu

--- 13-strapi-submenus/final/src/Submenu.jsx
+++ 13-strapi-submenus/final/src/Submenu.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import sublinks from './data';
+import { useGlobalContext } from './Context';
 
 const Submenu = () => {
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
   return (
-    <div>
-      <h2>submenu</h2>
+    <div className='submenu'>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className='submenu-links'
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr ifr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 };
