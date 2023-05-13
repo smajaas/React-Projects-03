9775d6a7ec64ebb0c722ae561cd3a5ddc0a5bcf1
smajaas <smajaas@gmail.com>
2023-05-14 00:17:32 +0530

    pageid logic

--- 13-strapi-submenus/final/src/Navlinks.jsx
+++ 13-strapi-submenus/final/src/Navlinks.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import sublinks from './data';
+import { useGlobalContext } from './Context';
 
 const Navlinks = () => {
+  const { setPageId } = useGlobalContext();
   return (
     <div className='nav-links'>
       {sublinks.map((item) => {
         const { pageId, page } = item;
         return (
-          <button key={pageId} className='nav-link'>
+          <button
+            key={pageId}
+            className='nav-link'
+            onMouseEnter={() => setPageId(pageId)}
+          >
             {page}
           </button>
         );
