08e02029bf82c04f3c0cc81156512efd824e6b1a
smajaas <smajaas@gmail.com>
2023-05-13 18:45:21 +0530

    global context

--- 13-strapi-submenus/final/src/App.jsx
+++ 13-strapi-submenus/final/src/App.jsx
@@ -1,4 +1,8 @@
+import { useGlobalContext } from './context';
+
 const App = () => {
+  const { isSidebarOpen } = useGlobalContext();
+  console.log(isSidebarOpen);
   return <h2>Strapi Starter</h2>;
 };
 export default App;
