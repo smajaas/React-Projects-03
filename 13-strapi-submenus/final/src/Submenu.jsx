527ce9d29e5dadd587dd22cb82c763fa4ac4a2c9
smajaas <smajaas@gmail.com>
2023-05-14 01:01:23 +0530

    submenu jsx and done

--- 13-strapi-submenus/final/src/Submenu.jsx
+++ 13-strapi-submenus/final/src/Submenu.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import sublinks from './data';
 import { useGlobalContext } from './Context';
-
+import sublinks from './data';
+import { useRef } from 'react';
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
 
+  const submenuContainer = useRef(null);
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
   return (
-    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+    <div
+      className={currentPage ? 'submenu show-submenu' : 'submenu'}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className='submenu-links'
         style={{
           gridTemplateColumns:
-            currentPage?.links?.length > 3 ? '1fr ifr' : '1fr',
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
         }}
       >
         {currentPage?.links?.map((link) => {
@@ -29,5 +43,4 @@ const Submenu = () => {
     </div>
   );
 };
-
 export default Submenu;
