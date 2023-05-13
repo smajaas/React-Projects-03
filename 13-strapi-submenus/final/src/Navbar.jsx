06463673006ee19b2af34b7ecba62c9c2c0468ee
smajaas <smajaas@gmail.com>
2023-05-14 00:10:58 +0530

    navlink componenet and css

--- 13-strapi-submenus/final/src/Navbar.jsx
+++ 13-strapi-submenus/final/src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import { useGlobalContext } from './Context';
+import Navlinks from './Navlinks';
 
 const Navbar = () => {
   const { openSidebar } = useGlobalContext();
@@ -11,7 +12,7 @@ const Navbar = () => {
         <button className='toggle-btn' onClick={openSidebar}>
           <FaBars />
         </button>
-        {/* nav links later */}
+        <Navlinks />
       </div>
     </nav>
   );
