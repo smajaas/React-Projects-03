9bcdc4789a34ebc28a73ad4f672201990596ff4f
smajaas <smajaas@gmail.com>
2023-05-16 12:57:38 +0530

    fetch structure

--- 14-cart/final/src/context.jsx
+++ 14-cart/final/src/context.jsx
@@ -10,11 +10,12 @@ import {
   DISPLAY_ITEMS,
 } from './actions';
 import { getTotals } from './utils';
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
 
 const AppContext = createContext();
 
 const initialState = {
-  loading: false,
+  loading: true,
   cart: new Map(cartItems.map((item) => [item.id, item])),
 };
 
@@ -36,6 +37,14 @@ export const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } });
   };
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const cart = await response.json();
+    console.log(cart);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <AppContext.Provider
       value={{
